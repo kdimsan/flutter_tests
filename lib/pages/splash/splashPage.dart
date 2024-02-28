f02fcd493eebe6e41dff1745f124c98a42b03428
kdimsan <kdimsantana@gmail.com>
2024-02-28 19:34:22 +0000

    splash page

---
+++ lib/pages/splash/splashPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/common/constants/app_colors.dart';
+import 'package:test_app/common/constants/app_text_styles.dart';
+import 'package:test_app/common/constants/logo_text.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.gradientSplashBg,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            LogoText(),
+            CircularProgressIndicator(
+              color: AppColors.whiteOne,
+            ),
+            Text(
+              'Splash Page',
+              style: AppTextStyles.bigText50w700
+                  .copyWith(color: AppColors.whiteOne),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
