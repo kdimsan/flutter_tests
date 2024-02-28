2e4b79407b7280c4f01c9b63176137f0a68d9511
kdimsan <kdimsantana@gmail.com>
2024-02-28 19:34:38 +0000

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: use_super_parameters, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:test_app/pages/sign_up/singUpPage.dart';
+import 'package:test_app/pages/splash/splashPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,57 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PageOne(),
-    );
-  }
-}
-
-class PageOne extends StatelessWidget {
-  const PageOne({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Page One'),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: Text('Go to second page'),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return SecondPage();
-              }));
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Second Page'),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: Text('Back to Page One'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Autentication',
+      home: SignUpPage(),
     );
   }
 }
