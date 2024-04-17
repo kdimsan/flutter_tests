c9b7693cc7b78a758f7b5abb14dd8d06456dba88
kdimsan <kdimsantana@gmail.com>
2024-04-17 11:11:13 +0100

    create routes tests

---
+++ lib/common/routes/routes.dart
@@ -0,0 +1,8 @@
+class MappedRoutes {
+  MappedRoutes._();
+
+  static const String initialPage = "/";
+  static const String onBoardingPage = "boarding";
+  static const String signUpPage = "sign_up";
+  static const String signInPage = "sign_in";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:test_app/common/constants/app_colors.dart';
-import 'package:test_app/pages/sign_up/sing_up_page.dart';
+import 'package:test_app/common/routes/routes.dart';
+import 'package:test_app/homePage.widget.dart';
+import 'package:test_app/pages/on_boarding/on_boarding_page.dart';
+import 'package:test_app/pages/sign_in/sign_in_page.dart';
+import 'package:test_app/pages/sign_up/sign_up_page.dart';
+import 'package:test_app/pages/splash/splashPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,7 +25,13 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       title: 'Autentication',
-      home: SignUpPage(),
+      initialRoute: MappedRoutes.initialPage,
+      routes: {
+        MappedRoutes.initialPage: (context) => const SplashPage(),
+        MappedRoutes.onBoardingPage: (context) => const OnBoardingPage(),
+        MappedRoutes.signUpPage: (context) => const SignUpPage(),
+        MappedRoutes.signInPage: (context) => const SignInPage(),
+      },
     );
   }
 }
